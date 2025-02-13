b0a52878909ac041b24139bd7d20cccf24284146
JavierPandal <A01028801@tec.mx>
2025-02-13 16:16:10 -0600

    verificación

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ area = math.pi * radio ** 2
 print(f"El perímetro de la circunferencia es {perimetro}")
 print(f"El área de la circunferencia es {area}")
 print(f"radio: {radio:.2f}")
+
+
+volumen = (4/3) * math.pi * radio ** 3
+print(f"El volumen de la esfera es {volumen}")
